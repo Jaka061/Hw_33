21497a30acaaa6237e0a07065f939835ca741306
Jaka061 <jrkny700@gmail.com>
2020-04-07 13:34:16 +0600

    hw_32

---
+++ Main_1.java
@@ -0,0 +1,14 @@
+package hw32;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main_1 {
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter("Theory-32");
+        fileWriter.write("");
+
+        fileWriter.close();
+
+    }
+}
